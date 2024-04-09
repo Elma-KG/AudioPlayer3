2622dffd8414099b430ae80d4ecac2920f1882c4
Elma Karen <ekg22@hi.is>
2024-04-09 10:12:57 +0000

    lagTest added

--- src/test/java/lagTest.java
+++ src/test/java/lagTest.java
@@ -11,5 +11,28 @@ public class lagTest {
 
     private Lag lagid;
 
+    @Before
+    public void setUp() {
+        lagid = new Lag("slod", "mynd", "nafn", 10);
+    }
+
+    @Test
+    public void testHljodskraNafn() {
+        assertEquals("slod", lagid.getHljodskraNafn());
+    }
+
+    @Test
+    public void testLagNafn() {
+        assertEquals("nafn", lagid.getLagNafn());
+    }
+    @Test
+    public void testMyndskraNafn() {
+        assertEquals("mynd", lagid.getMyndskraNafn());
+    }
+
+    @Test
+    public void testLengd() {
+        assertEquals(10, lagid.getLengd());
+    }
 
 }
